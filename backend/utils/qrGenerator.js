2343813541df5e6d3cf5dc24c31ca094b64ef878
Joelmore515 <joelmore515@gmail.com>
2026-08-11 11:11:35 -0500

    modificacion en la libreria de qr

--- backend/utils/qrGenerator.js
+++ backend/utils/qrGenerator.js
@@ -1,11 +1,12 @@
 const QRCode = require('qrcode');
-const { v4: uuidv4 } = require('uuid');
 
 /**
  * Genera un UUID único y su representación en código QR (Base64)
  */
 const generateQR = async () => {
+  const { v4: uuidv4 } = await import('uuid');
   const uuid = uuidv4();
+
   try {
     const qrDataURL = await QRCode.toDataURL(uuid);
     return { uuid, qrDataURL };
